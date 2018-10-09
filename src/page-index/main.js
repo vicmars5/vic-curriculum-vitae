1ecb677f1e70dbc15df8e5c486e0e0a6ee899f49
Victor Diaz <mars.vic5@gmail.com>
2018-10-09 02:21:10 -0500

    basic design template

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,5 +1,6 @@
 require('normalize.css/normalize.css');
 require('bootstrap/dist/css/bootstrap.css');
+require('../css/main.scss');
 require('./page.css');
 
 document.addEventListener("DOMContentLoaded", () => {
--- webpack.dev.js
+++ webpack.dev.js
@@ -9,8 +9,8 @@ module.exports = {
   // https://webpack.js.org/concepts/entry-points/#multi-page-application
   entry: {
     index: './src/page-index/main.js',
-    about: './src/page-about/main.js',
-    contacts: './src/page-contacts/main.js'
+    // about: './src/page-about/main.js',
+    // contacts: './src/page-contacts/main.js'
   },
 
   // https://webpack.js.org/configuration/dev-server/
@@ -38,6 +38,15 @@ module.exports = {
         ]
       },
       {
+        test: /\.scss$/,
+        use: [
+          "style-loader",
+          "css-loader",
+          "sass-loader"
+          // Please note we are not running postcss here
+        ]
+      },
+      {
         // Load all images as base64 encoding if they are smaller than 8192 bytes
         test: /\.(png|jpg|gif|svg)$/,
         use: [
@@ -61,8 +70,8 @@ module.exports = {
       inject: true,
       chunks: ['index'],
       filename: 'index.html'
-    }),
-    new HtmlWebpackPlugin({
+    })
+    /* new HtmlWebpackPlugin({
       template: './src/page-about/tmpl.html',
       inject: true,
       chunks: ['about'],
@@ -73,6 +82,6 @@ module.exports = {
       inject: true,
       chunks: ['contacts'],
       filename: 'contacts.html'
-    })
+    }) */
   ]
 };
